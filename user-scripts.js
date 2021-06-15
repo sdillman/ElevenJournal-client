fa64379fcfe13cf36d190bc3f4947cdc976849a7
Shannon D <sdillman@users.noreply.github.com>
2021-06-15 00:06:38 -0400

    2.2 CORS handling added

--- user-scripts.js
+++ user-scripts.js
@@ -2,8 +2,38 @@
 *** USER SIGNUP ***
 ************************** */
 function userSignUp() {
-     console.log('userSignUp Function Called')
-    }
+    console.log('userSignUp Function Called')
+
+    let userEmail = document.getElementById("emailSignup").value;
+    let userPass = document.getElementById("pwdSignup").value;
+
+    let newUserData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+    console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
+
+    fetch(`http://localhost:3000/user/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUserData)
+    })
+    .then(response => response.json())
+    .then (data => {
+        console.log(data);
+        let token = data.sessionToken;
+        localStorage.setItem('SessionToken', token);
+        tokenChecker();
+    })
+    .catch (err => {
+        console.log(err)
+    })
+};
+
     
     
     /* *************************
