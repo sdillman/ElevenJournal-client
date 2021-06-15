c2d839a43d90e606246e6e60d75875adda4726da
Shannon D <sdillman@users.noreply.github.com>
2021-06-15 00:42:42 -0400

    Through 2.5 LOGOUT

--- user-scripts.js
+++ user-scripts.js
@@ -15,6 +15,8 @@ function userSignUp() {
     };
     console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
 
+
+
     fetch(`http://localhost:3000/user/register`, {
         method: "POST",
         headers: {
@@ -40,15 +42,53 @@ function userSignUp() {
     *** USER LOGIN ***
     ************************** */
     function userLogin() {
-     console.log('userLogin Function Called')
-    }
+     console.log('userLogin Function Called');
+
+    let userEmail = document.getElementById('emailLogin').value;
+    let userPass = document.getElementById('pwdLogin').value;
+    console.log(userEmail, userPass);
+
+    let userData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+    console.log("userData -> ", userData);
+    console.log(`userData --> ${userData.user.email} ${userData.user.password}`);
+
+
+
+    fetch(`http://localhost:3000/user/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then (data => {
+        console.log(data);
+        let token = data.sessionToken;
+        localStorage.setItem('SessionToken', token);
+        tokenChecker();
+    })
+    .catch (err => {
+        console.error(err);
+    })
+
+    };
     
     
     /* *************************
     *** USER LOGOUT ***
     ************************** */
     function userLogout() {
-     console.log('userLogout Function Called')
+        console.log('userLogout Function Called');
+
+        localStorage.setItem('SessionToken', undefined);
+        console.log(`SessionToken --> ${localStorage.SessionToken}`);
+        tokenChecker();
     }
     
     
