71efa2641198148e97c28869db226e2608061dd9
Shannon D <sdillman@users.noreply.github.com>
2021-06-21 23:55:37 -0400

    finished remaining modules this time for reals includes the token checker function to provide feedback to the user about their logged-in status

--- user-scripts.js
+++ user-scripts.js
@@ -96,6 +96,24 @@ function userSignUp() {
      *** TOKEN CHECKER FUNCTION ***
     ************************** */
     function tokenChecker() {
-     console.log('tokenChecker Function Called')
+     console.log('tokenChecker Function Called');
+
+    let display = document.getElementById('journals');
+    let header = document.createElement('h5');
+    let accessToken = localStorage.getItem('SessionToken');
+    let alertText = "Log in or sign up to get started!";
+
+    for (let i=0; i<display.childNodes.length; i++) {
+        display.removeChild(display.firstChild);
     }
-    tokenChecker()
+
+    if (accessToken === 'undefined') {
+        display.appendChild(header);
+        header.textContent = alertText;
+        header.setAttribute('id', 'defaultLogin');
+    } else {
+        null
+    }
+
+    }
+    tokenChecker();
